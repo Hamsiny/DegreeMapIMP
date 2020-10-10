74944c6b01ac3a926e627f048b4aff77b10949fb
Olivia <jamreya9@student.wintec.ac.nz>
2020-10-10 18:30:48 +1300

    Student object variables

---
+++ app/src/main/java/com/randomone/androidmonsterc3/Student.java
@@ -0,0 +1,9 @@
+package com.randomone.androidmonsterc3;
+
+public class Student {
+    public String FName;
+    public String LName;
+    public String Phone;
+    public String Email;
+    public String PhotoURL;
+}
