adbaf7e15674af39d1a3d275aaaffbb2a1172b78
Olivia <jamreya9@student.wintec.ac.nz>
2020-10-10 15:22:19 +1300

    Added Map type to lists
    pathway, prerequisite, corequisite should be accessible now

--- app/src/main/java/com/randomone/androidmonsterc3/Module.java
+++ app/src/main/java/com/randomone/androidmonsterc3/Module.java
@@ -1,41 +1,94 @@
 package com.randomone.androidmonsterc3;
 
 
+import java.util.Map;
+
 public class Module {
-    private String Title;
-    private String Description;
-    private int Credits;
-    private int Level;
-    private String Time;
-    private Pathway Pathway;
-    private Prerequisite Prerequisites;
-    private Corequisite Corequisites;
+    public String Title;
+    public String Description;
+    public String Time;
+    public int Credits;
+    public int Level;
+    public Map<String, Boolean> Pathway;
+    public Map<String, Boolean> Prerequisite;
+    public Map<String, Boolean> Corequisite;
 
     public Module(){
 
     }
 
-    public Module(String title, String description, int credits, int level, String time, Pathway pathway, Prerequisite prerequisites, Corequisite corequisites) {
+    public Module(String title, String description, String time, int credits, int level, Map<String, Boolean> pathway, Map<String, Boolean> prerequisite, Map<String, Boolean> corequisite) {
         Title = title;
         Description = description;
+        Time = time;
         Credits = credits;
         Level = level;
-        Time = time;
         Pathway = pathway;
-        Prerequisites = prerequisites;
-        Corequisites = corequisites;
+        Prerequisite = prerequisite;
+        Corequisite = corequisite;
     }
 
-    public class Pathway {
-
+    public String getTitle() {
+        return Title;
     }
 
-    public class Prerequisite {
-
+    public void setTitle(String title) {
+        Title = title;
     }
 
-    public class Corequisite {
-
+    public String getDescription() {
+        return Description;
     }
 
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getTime() {
+        return Time;
+    }
+
+    public void setTime(String time) {
+        Time = time;
+    }
+
+    public int getCredits() {
+        return Credits;
+    }
+
+    public void setCredits(int credits) {
+        Credits = credits;
+    }
+
+    public int getLevel() {
+        return Level;
+    }
+
+    public void setLevel(int level) {
+        Level = level;
+    }
+
+    public Map<String, Boolean> getPathway() {
+        return Pathway;
+    }
+
+    public void setPathway(Map<String, Boolean> pathway) {
+        Pathway = pathway;
+    }
+
+    public Map<String, Boolean> getPrerequisite() {
+        return Prerequisite;
+    }
+
+    public void setPrerequisite(Map<String, Boolean> prerequisite) {
+        Prerequisite = prerequisite;
+    }
+
+    public Map<String, Boolean> getCorequisite() {
+        return Corequisite;
+    }
+
+    public void setCorequisite(Map<String, Boolean> corequisite) {
+        Corequisite = corequisite;
+    }
 }
