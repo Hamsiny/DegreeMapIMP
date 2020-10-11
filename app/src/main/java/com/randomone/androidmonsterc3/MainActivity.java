27053db54c0b040345035fbdaf4de612708bad09
Vincent <459516309@qq.com>
2020-10-11 19:39:38 +1300

    Add json file, add splash screen activity, set style to no action bar, change the default activity in manifest

--- app/src/main/java/com/randomone/androidmonsterc3/MainActivity.java
+++ app/src/main/java/com/randomone/androidmonsterc3/MainActivity.java
@@ -3,7 +3,6 @@ package com.randomone.androidmonsterc3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,13 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
-    private void onclick(){
-        Toast.makeText(this, "You click me!", Toast.LENGTH_SHORT).show();
-    }
-
-    private void onclick2(){
-        Toast.makeText(this, "Toast", Toast.LENGTH_SHORT).show();
-    }
 }
