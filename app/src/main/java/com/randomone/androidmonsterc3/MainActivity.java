053a8b824a7b9ed9da311856091db9ff1628e5cb
Olivia <jamreya9@student.wintec.ac.nz>
2020-10-14 11:38:56 +1300

    Moved Navbar to ModuleActivity
    adding user type based xml

--- app/src/main/java/com/randomone/androidmonsterc3/MainActivity.java
+++ app/src/main/java/com/randomone/androidmonsterc3/MainActivity.java
@@ -1,41 +1,19 @@
 package com.randomone.androidmonsterc3;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.os.Bundle;
 import android.widget.Toast;
-import androidx.appcompat.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
-    private DrawerLayout drawer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
     }
 
-    @Override
-    public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        }
-        else {
-            super.onBackPressed();
-        }
-    }
 
     private void onclick(){
         Toast.makeText(this, "You click me!", Toast.LENGTH_SHORT).show();
