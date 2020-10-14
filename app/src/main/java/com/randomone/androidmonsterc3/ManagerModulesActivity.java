1d233fcd59bc7eea5e5e8bfb0ea3571fa9d6da87
Vincent <459516309@qq.com>
2020-10-14 17:16:47 +1300

    Add manager and student activity, add entrance activity for student and manager, add popup password input for manager entrance(will be replaced by new activity), change module cardview style, add yellow frame shape to cardview, modify details of splash screen.

---
+++ app/src/main/java/com/randomone/androidmonsterc3/ManagerModulesActivity.java
@@ -0,0 +1,19 @@
+package com.randomone.androidmonsterc3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManagerModulesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modules_manager);
+
+        //add logo on action bar
+        /*getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.androidmonsterlogo);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);*/
+    }
+}
