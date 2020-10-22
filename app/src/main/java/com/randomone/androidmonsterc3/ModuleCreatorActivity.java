6ffdbc1b0f3328eaa7296ac41b852851fb84d9e3
Olivia <jamreya9@student.wintec.ac.nz>
2020-10-22 21:00:16 +1300

    Add Module XML Created

---
+++ app/src/main/java/com/randomone/androidmonsterc3/ModuleCreatorActivity.java
@@ -0,0 +1,24 @@
+package com.randomone.androidmonsterc3;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ModuleCreatorActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Spinner spinner = findViewById(R.id.time_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource
+                (this, R.array.time_picker_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+    }
+}
