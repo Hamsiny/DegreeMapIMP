bed581a67350effa6a6deb82e98045444029a8ff
Olivia <jamreya9@student.wintec.ac.nz>
2020-10-28 17:21:13 +1300

    Rewrote Student method and implemented Parcelable

--- app/src/main/java/com/randomone/androidmonsterc3/Student.java
+++ app/src/main/java/com/randomone/androidmonsterc3/Student.java
@@ -1,62 +1,111 @@
 package com.randomone.androidmonsterc3;
 
-public class Student {
-    public String FName;
-    public String LName;
-    public String Phone;
-    public String Email;
-    public String PhotoURL;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Student implements Parcelable {
+    public String fName;
+    public String lName;
+    public int studentID;
+    public String phone;
+    public String email;
+    public String photoURL;
 
     public Student() {
         //Empty Constructor
     }
 
-    public Student(String FName, String LName, String phone, String email, String photoURL) {
-        this.FName = FName;
-        this.LName = LName;
-        this.Phone = phone;
-        this.Email = email;
-        this.PhotoURL = photoURL;
+    public Student(String fName, String lName, int studentID, String phone, String email, String photoURL) {
+        this.fName = fName;
+        this.lName = lName;
+        this.studentID = studentID;
+        this.phone = phone;
+        this.email = email;
+        this.photoURL = photoURL;
     }
 
-    public String getFName() {
-        return FName;
+    protected Student(Parcel in) {
+        fName = in.readString();
+        lName = in.readString();
+        studentID = in.readInt();
+        phone = in.readString();
+        email = in.readString();
+        photoURL = in.readString();
     }
 
-    public void setFName(String FName) {
-        this.FName = FName;
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(fName);
+        dest.writeString(lName);
+        dest.writeInt(studentID);
+        dest.writeString(phone);
+        dest.writeString(email);
+        dest.writeString(photoURL);
     }
 
-    public String getLName() {
-        return LName;
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
-    public void setLName(String LName) {
-        this.LName = LName;
+    public static final Creator<Student> CREATOR = new Creator<Student>() {
+        @Override
+        public Student createFromParcel(Parcel in) {
+            return new Student(in);
+        }
+
+        @Override
+        public Student[] newArray(int size) {
+            return new Student[size];
+        }
+    };
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
     }
 
     public String getPhone() {
-        return Phone;
+        return phone;
     }
 
     public void setPhone(String phone) {
-        this.Phone = phone;
+        this.phone = phone;
     }
 
     public String getEmail() {
-        return Email;
+        return email;
     }
 
     public void setEmail(String email) {
-        this.Email = email;
+        this.email = email;
     }
 
     public String getPhotoURL() {
-        return PhotoURL;
+        return photoURL;
     }
 
     public void setPhotoURL(String photoURL) {
-        this.PhotoURL = photoURL;
+        this.photoURL = photoURL;
     }
 }
 
