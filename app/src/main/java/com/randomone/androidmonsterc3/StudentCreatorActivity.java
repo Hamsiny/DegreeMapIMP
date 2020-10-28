7c6766acd081317f412e59d4106c307454101395
Olivia <jamreya9@student.wintec.ac.nz>
2020-10-28 22:26:43 +1300

    upload half done

--- app/src/main/java/com/randomone/androidmonsterc3/StudentCreatorActivity.java
+++ app/src/main/java/com/randomone/androidmonsterc3/StudentCreatorActivity.java
@@ -1,23 +1,40 @@
 package com.randomone.androidmonsterc3;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
 
 public class StudentCreatorActivity extends AppCompatActivity {
     private static final int PICK_IMAGE_REQUEST = 1;
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference mStorageRef;
+    StorageTask mStorageTask;
 
     private EditText mFirstname, mLastname, mPhone, mEmail;
     private Button mUploadButton;
     private ImageView mProfileImage;
-
+    private Uri mImageUri;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,6 +47,8 @@ public class StudentCreatorActivity extends AppCompatActivity {
         mEmail = findViewById(R.id.student_email);
         mUploadButton = findViewById(R.id.student_upload_image);
         mProfileImage = findViewById(R.id.student_image);
+
+        mStorageRef = storage.getReference("studentImages");
 
         //open file dialog for profile image
         mUploadButton.setOnClickListener(new View.OnClickListener() {
@@ -44,4 +63,48 @@ public class StudentCreatorActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            mImageUri = data.getData();
+            Glide.with(this).load(mImageUri).centerCrop().into(mProfileImage);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.create_student_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_module:
+                uploadStudent();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void uploadStudent() {
+        if (mImageUri != null) {
+            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()
+                    + "." + getFileExtension(mImageUri));
+        }
+
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+
 }
